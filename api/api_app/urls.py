9fefad519215f08ae285b1557cdd4f137205cf11
Edmar D. O. Filho <87383421+Edmar8111@users.noreply.github.com>
2024-10-09 12:18:56 -0300

    Delete api directory

--- api/api_app/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from api_app.views import FundoImobiliarioViewSet
-
-app_name = 'api'
-router = DefaultRouter(trailing_slash=False)
-router.register(r'fundos', FundoImobiliarioViewSet)
-urlpatterns=router.urls
